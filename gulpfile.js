22c11523687457d63c7634dfb5a2340668610427
Doc <abigguy4u@gmx.com>
2018-03-12 19:14:07 +0100

    Commit: 2018 March 12

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
+
+gulp.task('default', function(){
+    console.log('Gulp Task created.');
+});
+
+gulp.task('html', function(){
+    console.log('index.html has been changed.');
+});
+
+gulp.task('styles', function(){
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe()
+        .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles');
+    });
+});
