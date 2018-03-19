a9ba6e597f44f71918196b830ecf1dcf930ca208
Doc <abigguy4u@gmx.com>
2018-03-19 16:58:27 +0100

    Section 8, Lecture 26 (done)

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+// Watch Task: combining all Tasks and run together
+gulp.task('watch', function(){
+
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
+
+    watch('./app/index.html', function(){
+        browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('cssInject');
+    });
+});
+
+// Inject CSS into file.html without reloading browser
+gulp.task('cssInject', ['styles'], function(){
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
