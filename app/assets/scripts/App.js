095cd914d135b862a4d02b382c850b5e9f1c6968
Doc <abigguy4u@gmx.com>
2018-03-31 21:58:01 +0200

    Completed sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,27 +1,7 @@
 import $ from "jquery";
-
-// var $ = require('jquery');
-// var Person = require('./modules/Person');
-// import Person from './modules/Person';
-
-// class Adult extends Person {
-//     payTaxes() {
-//         console.log(this.name + " now owes: $0 in taxes.");
-//     }
-// }
-
-
-// var john = new Person('John Doe', 'blue');
-// john.greet();
-
-// var jane = new Adult('Jane Smith', 'orange');
-// jane.greet();
-// jane.payTaxes();
-
-// $("h1").remove();
-
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 // var revealOnScroll = new RevealOnScroll();
@@ -30,3 +10,5 @@ new RevealOnScroll($(".feature-item"), "85%");
 
 // Testimonials
 new RevealOnScroll($(".testimonial"), "65%");
+
+var stickyHeader = new StickyHeader();
